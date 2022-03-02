be92a28bef0d24196ce0859bb61c6ba265832525
rknob <rknob@inpulse.vet>
2022-03-02 11:47:27 -0300

    get script path

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use std::borrow::Cow;
-use std::fmt::format;
-use std::fs::File;
-use std::ops::Deref;
+extern crate dirs;
+
 use std::process::Command;
 use std::sync::{Arc, LockResult, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
-use eframe::egui::{CentralPanel, Pos2, ProgressBar, RichText, WidgetText};
-use eframe::{epi, NativeOptions};
-use eframe::epaint::util::Float;
-use eventuals::{Eventual, EventualWriter, Value};
-use crate::epi::egui::{Context, Rgba, Ui, Vec2};
-use crate::epi::{Frame, IconData, Storage};
 
-use reqwest::blocking::*;
+use dirs::home_dir;
+use eframe::{epi, NativeOptions};
+use eframe::egui::{CentralPanel, Pos2, ProgressBar, RichText, WidgetText};
+use eventuals::{Eventual, EventualWriter, Value};
+
+use crate::epi::{Frame, IconData, Storage};
+use crate::epi::egui::{Context, Rgba, Ui, Vec2};
+
 
 #[derive(PartialEq, Clone)]
 struct AppState {
@@ -51,9 +50,6 @@ impl epi::App for App {
         "Hello"
     }
 
-    // fn clear_color(&self) -> Rgba {
-    //     Rgba::from_rgb(255.0, 255.0, 255.0)
-    // }
 }
 
 fn update_value<T>(now: &Eventual<T>, writer: &mut EventualWriter<T>, update: impl FnOnce(&mut T)) where T: Value {
@@ -62,6 +58,19 @@ fn update_value<T>(now: &Eventual<T>, writer: &mut EventualWriter<T>, update: im
         writer.write(value);
     }
 
+}
+
+fn get_script_path() -> Option<String> {
+    let relative_path = String::from("Downloads");
+    let script_name = String::from("test_script.sh");
+
+    if let Some(user_home_path_buf) = dirs::home_dir() {
+        let user_home_path = user_home_path_buf.display().to_string();
+        let print_path = format!("{}/{}/{}", user_home_path, relative_path, script_name);
+        return Some(print_path);
+    } else {
+        None
+    }
 }
 
 fn main() {
@@ -90,39 +99,40 @@ fn main() {
             sleep(Duration::from_millis(250));
         }
 
-        let output = Command::new("ls")
-            .arg("-la")
-            .output()
-            .expect("Failed to execute command");
 
-        update_value(&event, &mut writer, move |mut value| {
-            value.progress += 0.1;
-            value.text = String::from_utf8(output.stdout).unwrap();
-        });
+        if let Some(script_path) = get_script_path() {
+            let output = Command::new(script_path)
+                .output()
+                .expect("Failed");
 
-        let client = reqwest::blocking::ClientBuilder::default().build().unwrap();
-
-        let download_url = "https://releases.ubuntu.com/20.04.3/ubuntu-20.04.3-desktop-amd64.iso";
-        let get_size_request = client.head(download_url)
-            .build().unwrap();
-
-        let size: u64 = client.execute(get_size_request).unwrap()
-            .headers().get(reqwest::header::CONTENT_LENGTH).unwrap()
-            .to_str().unwrap()
-            .parse().unwrap();
-
-        let download_path = std::path::Path::new("/tmp/ubuntu");
-        if download_path.exists() {
-            std::fs::remove_file(download_path).unwrap();
+            update_value(&event, &mut writer, move |mut value| {
+                value.text = String::from_utf8(output.stdout).unwrap();
+            });
         }
 
-        // client.execute(client.get(download_url)
-        //     .build().unwrap()).unwrap().;
 
-        update_value(&event, &mut writer, move |mut value| {
-            value.text = format!("size: {}", size);
-        });
+        // let output = Command::new(script_path)
+        //     .output()
+        //     .expect("Failed");
+
+        // let output = Command::new("ls")
+        //     // .current_dir("$HOME")
+        //     // .arg("/Downloads/test_script.sh")
+        //     .output()
+        //     .expect("Failed to execute command");
+
+        // update_value(&event, &mut writer, move |mut value| {
+        //     value.progress += 0.1;
+        //     value.text = String::from_utf8(output.stdout).unwrap();
+        // });
+
+        // println!("{}", String::from_utf8(output.stdout).unwrap());
+        // update_value(&event, &mut writer, move |mut value| {
+        //     value.text = format!("size: {}", size);
+        // });
     });
+
+
     eframe::run_native(Box::new(app), native_options);
 }
 
